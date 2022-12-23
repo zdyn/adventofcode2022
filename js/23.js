965ae7dadeb48e50a7cc1778277076f35adc4795
Jing Brian Zhou <bzthemusician@gmail.com>
2022-12-23 00:51:04 -0600

    Shorten

--- js/23.js
+++ js/23.js
@@ -1,42 +1,9 @@
 export const fns = {
   "Part 1": (input) => {
-    const elves = new Set();
-    input
-      .trim()
-      .split("\n")
-      .forEach((row, i) => {
-        row.split("").forEach((c, j) => {
-          if (c === "#") elves.add(key(i, j));
-        });
-      });
+    const elves = parse(input);
     let dirs = "NSWE";
     for (let i = 0; i < 10; i++) {
-      const moves = {};
-      for (const k of elves) {
-        const [x, y] = fromKey(k);
-        if (!occupied(elves, neighbors(x, y))) continue;
-
-        const checks = [
-          neighbors(x, y, dirs[0]),
-          neighbors(x, y, dirs[1]),
-          neighbors(x, y, dirs[2]),
-          neighbors(x, y, dirs[3]),
-        ];
-        for (const check of checks) {
-          if (!occupied(elves, check)) {
-            const to = key(...check[1]);
-            moves[to] ??= [];
-            moves[to].push(k);
-            break;
-          }
-        }
-      }
-      for (const [k, v] of Object.entries(moves)) {
-        if (v.length > 1) continue;
-
-        elves.delete(v[0]);
-        elves.add(k);
-      }
+      move(elves, dirs);
       dirs = dirs.slice(1) + dirs[0];
     }
     let min = [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER];
@@ -49,52 +16,63 @@ export const fns = {
     return (max[0] - min[0] + 1) * (max[1] - min[1] + 1) - elves.size;
   },
   "Part 2": (input) => {
-    const elves = new Set();
-    input
-      .trim()
-      .split("\n")
-      .forEach((row, i) => {
-        row.split("").forEach((c, j) => {
-          if (c === "#") elves.add(key(i, j));
-        });
-      });
+    const elves = parse(input);
     let dirs = "NSWE";
     let rounds = 0;
-    let done = false;
-    while (!done) {
+    while (true) {
       rounds++;
-      done = true;
-      const moves = {};
-      for (const k of elves) {
-        const [x, y] = fromKey(k);
-        if (!occupied(elves, neighbors(x, y))) continue;
-
-        const checks = [
-          neighbors(x, y, dirs[0]),
-          neighbors(x, y, dirs[1]),
-          neighbors(x, y, dirs[2]),
-          neighbors(x, y, dirs[3]),
-        ];
-        for (const check of checks) {
-          if (!occupied(elves, check)) {
-            const to = key(...check[1]);
-            moves[to] ??= [];
-            moves[to].push(k);
-            break;
-          }
-        }
-      }
-      for (const [k, v] of Object.entries(moves)) {
-        if (v.length > 1) continue;
-
-        done = false;
-        elves.delete(v[0]);
-        elves.add(k);
+      if (!move(elves, dirs)) {
+        break;
       }
       dirs = dirs.slice(1) + dirs[0];
     }
     return rounds;
   },
+};
+
+const parse = (input) => {
+  const elves = new Set();
+  input
+    .trim()
+    .split("\n")
+    .forEach((row, i) => {
+      row.split("").forEach((c, j) => {
+        if (c === "#") elves.add(key(i, j));
+      });
+    });
+  return elves;
+};
+
+const move = (elves, dirs) => {
+  const moves = {};
+  let changed = false;
+  for (const k of elves) {
+    const [x, y] = fromKey(k);
+    if (!occupied(elves, neighbors(x, y))) continue;
+
+    const checks = [
+      neighbors(x, y, dirs[0]),
+      neighbors(x, y, dirs[1]),
+      neighbors(x, y, dirs[2]),
+      neighbors(x, y, dirs[3]),
+    ];
+    for (const check of checks) {
+      if (!occupied(elves, check)) {
+        const to = key(...check[1]);
+        moves[to] ??= [];
+        moves[to].push(k);
+        break;
+      }
+    }
+  }
+  for (const [k, v] of Object.entries(moves)) {
+    if (v.length > 1) continue;
+
+    changed = true;
+    elves.delete(v[0]);
+    elves.add(k);
+  }
+  return changed;
 };
 
 const key = (...args) => args.join(",");
@@ -143,15 +121,6 @@ const neighbors = (x, y, dir) => {
 
 const occupied = (s, coords) => {
   return coords.some((coord) => s.has(key(...coord)));
-};
-
-Array.prototype.forMatrix = function (fn) {
-  for (let i = 0; i < this.length; i++) {
-    for (let j = 0; j < this[i].length; j++) {
-      const result = fn(this[i][j], i, j);
-      if (result != null) return result;
-    }
-  }
 };
 
 export const samples = `....#..
